c2092fc3703105c1fdd36d33b71230446c2fd772
Christian Chimezie <chris.chimezie@gmail.com>
2020-06-01 19:19:39 -0400

    Seperate each result into info and link.

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -1,29 +1,39 @@
 import styles from '../styles/SearchResults.module.css'
+import linkIcon from '../assets/images/iconmonstr-link-thin.svg'
 
 function SearchResults(props) {
 	if (props.results) {
 		const results = props.results.map(item => (
 			<div className={styles.result}>
-				<div className={styles.title}>
-					{item.title}
+				<div className={styles.info}>
+					<div className={styles.title}>
+						{item.title}
+					</div>
+					<div>
+						contributors: {item.contributors}
+					</div>
+					<div>
+						type: {item.type}
+					</div>
+					<div>
+						sources: {item.sources}
+					</div>
+					<div>
+						date_updated: {item.date_updated}
+					</div>
+					<div>
+						date_published: {item.date_published}
+					</div>
 				</div>
-				<div>
-					contributors: {item.contributors}
-				</div>
-				<div>
-					type: {item.type}
-				</div>
-				<div>
-					identifiers: <a href={item.identifiers[0]}>link</a>
-				</div>
-				<div>
-					sources: {item.sources}
-				</div>
-				<div>
-					date_updated: {item.date_updated}
-				</div>
-				<div>
-					date_published: {item.date_published}
+				<div className={styles.link}>
+					<a href={item.identifiers[0]}>
+						<div className={styles.innerLink}>
+							<img
+								className={styles.linkIcon}
+								src={linkIcon}
+							/>
+						</div>
+					</a>
 				</div>
 			</div>
 		))
