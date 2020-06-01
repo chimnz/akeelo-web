17df4043bd34ecd8fe1e32731298708c4daf8a1f
Christian Chimezie <chris.chimezie@gmail.com>
2020-06-01 17:12:32 -0400

    Style search page.

--- components/SearchResults.js
+++ components/SearchResults.js
@@ -2,10 +2,34 @@ import styles from '../styles/SearchResults.module.css'
 
 function SearchResults(props) {
 	if (props.results) {
-		const titles = props.results.map(item => <h1>{item.title}</h1>)
+		const results = props.results.map(item => (
+			<div className={styles.result}>
+				<div className={styles.title}>
+					{item.title}
+				</div>
+				<div>
+					contributors: {item.contributors}
+				</div>
+				<div>
+					type: {item.type}
+				</div>
+				<div>
+					identifiers: <a href={item.identifiers[0]}>link</a>
+				</div>
+				<div>
+					sources: {item.sources}
+				</div>
+				<div>
+					date_updated: {item.date_updated}
+				</div>
+				<div>
+					date_published: {item.date_published}
+				</div>
+			</div>
+		))
 		return (
 			<div className={styles.wrapper}>
-				{titles}
+				{results}
 			</div>
 		)
 	} else {
