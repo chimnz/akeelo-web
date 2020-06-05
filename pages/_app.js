85d35a0b97a4bd416b99f7ddcc18ae348dfe571d
Christian Chimezie <chris.chimezie@gmail.com>
2020-06-04 23:07:30 -0400

    Set website title.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
 import '../styles/global.css'
 
-function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />
+import App from 'next/app'
+import Head from 'next/head'
+import React from 'react'
+
+// https://github.com/vercel/next.js/blob/master/errors/no-document-title.md
+export default class MyApp extends App {
+  static async getInitialProps({ Component, ctx }) {
+    let pageProps = {}
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx)
+    }
+
+    return { pageProps }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <>
+        <Head>
+			<title>akeelo - search for science</title>
+        </Head>
+        <Component {...pageProps} />
+      </>
+    )
+  }
 }
-  
-export default MyApp
