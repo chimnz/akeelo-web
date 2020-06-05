730917e6c8db6b08ec52657346b6de418fb42cdc
Christian Chimezie <chris.chimezie@gmail.com>
2020-06-04 22:30:37 -0400

    Add nav links to homepage.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
 import styles from '../styles/HomePage.module.css'
 import { useRouter } from 'next/router'
 import searchIcon from '../assets/images/iconmonstr-search-thin.svg'
+
 import osfShareLogo from '../assets/images/osf-share-logo.png'
+import githubIcon from '../assets/images/iconmonstr-github-1.svg'
+import personalWebsiteLogo from '../assets/images/personal-website-icon.png'
 
 const apiDocsUrl = 'https://share-research.readthedocs.io'
+const githubUrl = 'https://github.com/chimnz/akeelo-web'
+const personalWebsiteUrl = 'https://hdqrs.co'
 
 function HomePage() {
 	const router = useRouter()
@@ -19,38 +24,51 @@ function HomePage() {
 	}
 
 	return (
-		<main>
-			<div className='main-inner'>
-				<div className={styles.spacer}></div>
-				<div className={styles.contentWrapper}>
-					<div className={styles.searchWrapper}>
-						<div className={styles.logo}>akeelo</div>
-						<form
-							className={styles.form}
-							onSubmit={goToSearchPage}
-						>
-							<input
-								className={styles.textInput}
-								ref={formInput}
-								type='text'
-								placeholder='search for science'
-							/>
-							<button
-								className={styles.submitButton}
-							>
-								<img src={searchIcon} />
-							</button>
-						</form>
-					</div>
-					<div className={styles.attribution}>
-						powered by
-						<a href={apiDocsUrl}>
-							<img src={osfShareLogo} />
+		<>
+			<nav className={styles.nav}>
+				<div>
+					<div className={styles.linkWrapper}>
+						<a href={personalWebsiteUrl}>
+							<img src={personalWebsiteLogo} />
+						</a>
+						<a href={githubUrl}>
+							<img src={githubIcon} />
 						</a>
 					</div>
 				</div>
-			</div>
-		</main>
+			</nav>
+			<main className={styles.main}>
+				<div className='main-inner'>
+					<div className={styles.contentWrapper}>
+						<div className={styles.searchWrapper}>
+							<div className={styles.logo}>akeelo</div>
+							<form
+								className={styles.form}
+								onSubmit={goToSearchPage}
+							>
+								<input
+									className={styles.textInput}
+									ref={formInput}
+									type='text'
+									placeholder='search for science'
+								/>
+								<button
+									className={styles.submitButton}
+								>
+									<img src={searchIcon} />
+								</button>
+							</form>
+						</div>
+						<div className={styles.attribution}>
+							powered by
+							<a href={apiDocsUrl}>
+								<img src={osfShareLogo} />
+							</a>
+						</div>
+					</div>
+				</div>
+			</main>
+		</>
 	)
 }
 
