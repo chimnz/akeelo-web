d50e579bf0cd6ed5b08582208ef2759ef6589504
Christian Chimezie <chris.chimezie@gmail.com>
2020-08-11 12:12:09 -0400

    chore: regen site

--- docs/_next/static/runtime/webpack.js
+++ docs/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "604830223c8efbd13186";
+/******/ 	var hotCurrentHash = "22eeb07d5ee471f2fe47";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
